4173d78a0e9f08d28159d916df0fa4c03ecf6fc1
Josh <joshuakettle1990@gmail.com>
2017-10-13 21:27:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ var updateObjectWithKeyAndValue = function(object, key, value){
 var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
   object[key]= value
 
-  return object 
+  return object
 }
