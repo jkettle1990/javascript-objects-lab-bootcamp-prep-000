bbd5c3ac47f4eaf5c4cff988f070b553e21d4d24
Josh <joshuakettle1990@gmail.com>
2017-10-13 21:26:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var recipes = {}
 var updateObjectWithKeyAndValue = function(object, key, value){
   return Object.assign({}, object, {[key]: value })
 }
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
+  object[key]= value
+
+  return object 
+}
